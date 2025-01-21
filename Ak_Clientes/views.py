2e224b872bcea8f3073e4af683c09a5b28fcd086
Federico Jose Cuda <fjcuda@gmail.com>
2025-01-21 03:23:38 -0300

    Agregue banda de cosas

--- Ak_Clientes/views.py
+++ Ak_Clientes/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    return render(request, 'Ak_Clientes/index.html')
+
+def fede(request):
+    return render(request,'Ak_Clientes/fede.html')  
